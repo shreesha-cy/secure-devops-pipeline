c4398f37288a1e68fc97a13b3f2b3da659e3c414
Anoop <anoopss6111@gmail.com>
2026-05-05 23:12:30 +0530

    Fixed Build jar

--- app/src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
+++ app/src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
@@ -16,7 +16,7 @@
 
 package org.springframework.samples.petclinic.system;
 
-import org.springframework.cache.jcache.JCacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
